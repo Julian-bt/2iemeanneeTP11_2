4756c0aaa6138c27364ff77ba39f66d2efe51737
Julian-bt <julian.burtin@isen-ouest.yncrea.fr>
2020-03-24 12:11:09 +0100

    TD11

---
+++ exercice1.py
@@ -0,0 +1,24 @@
+class Circle:
+    def __init__(self,rayon):
+        self.__rayon=rayon
+    def __add__(self, other):
+        return Circle(self.__rayon+other.__rayon) #ici on return un objet
+    def __lt__(self, other):
+        return self.__rayon<other.__rayon
+    def __gt__(self, C):
+        return self.__rayon > C.__rayon
+    def __str__(self):
+        return self.__rayon
+
+
+
+if __name__== '__main__':
+    c1 = Circle(2)
+    c2 = Circle(3)
+    c3 = c1 + c2 #on veut creer un nouvel objet donc on doit return un objet
+    c4 = c1 < c2
+    c5 = c2 > c3
+    print(c5)
+    print(isinstance(c1,Circle))
+    print(isinstance(c2,Circle))
+
---
+++ exercice2.py
@@ -0,0 +1,54 @@
+
+class Complex:
+    def __init__(self,re,im):
+        self.__re=re
+        self.__im=im
+    def __add__(self, other):
+        return Complex(self.__re + other.__re,self.__im + other.__im)
+    def __sub__(self, other):
+        return Complex(self.__re - other.__re,self.__im - other.__im)
+    def __mul__(self, other):
+        a=self.__re * other.__re
+        b=self.__im*other.__re
+        c=-self.__im*other.__im
+        d=self.__re*other.__im
+        return Complex(a+c,b+d)
+    def __truediv__(self, other):
+        if other.__re !=0 and other.__im!=0:
+            return Complex(self.__re / other.__re,self.__im / other.__im)
+    def __abs__(self):
+        return ((self.__re**2 + self.__im**2)**0.5)
+    def __eq__(self, other):
+        return self.__re== other.__im and self.__im == other.__im
+    def __ne__(self, Comp):
+       return (self.__re != Comp.__re or self.__im != Comp.__im)
+    def getre(self):
+        return self.__re
+    def getim(self):
+        return self.__im
+
+
+
+if __name__== '__main__':
+    c1=Complex(2,6)
+    c2=Complex(1,2)
+    C3=(c1+c2)
+    print(str(C3.getre())+"+"+str(C3.getim())+"i")
+
+    C4=(c1-c2)
+    print(str(C4.getre())+"+"+str(C4.getim())+"i")
+
+    C5=(c1*c2)
+    print(str(C5.getre())+"+"+str(C5.getim())+"i")
+
+    C6=abs(c1)
+    print(C6)
+
+    C7=(c1==c2)
+    print(C7)
+
+    C8= c1!=c2
+    print(C8)
+
+    print(isinstance(c1,Complex))
+    print(isinstance(c2,Complex))
